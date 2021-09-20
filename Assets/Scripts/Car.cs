805423d2d178d057d5ceec1422add377c7c654d3
nanaco541 <vre343434@gmail.com>
2021-09-20 16:34:28 +0800

    test transform.Rotate & EventTrigger

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -6,11 +6,21 @@ public class Car : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
     [SerializeField] private float speedGainPerSecond = 0.2f;
+    [SerializeField] private float turnSpeed = 200f;
+
+    private int steerValue;
 
     void Update()
     {
         speed += speedGainPerSecond * Time.deltaTime;
 
+        transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
+
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    public void Steer(int value)
+    {
+        steerValue = value;
     }
 }
