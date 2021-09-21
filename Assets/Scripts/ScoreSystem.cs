75a14289ec98e75170f8e9ec68b3af635613b28c
nanaco541 <vre343434@gmail.com>
2021-09-21 16:16:27 +0800

    add ScoreSystem

---
+++ Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private float scoreMultiplier;
+
+    private float score;
+
+    // Update is called once per frame
+    void Update()
+    {
+        score += Time.deltaTime * scoreMultiplier;
+
+        scoreText.text = Mathf.FloorToInt(score).ToString();
+    }
+}
