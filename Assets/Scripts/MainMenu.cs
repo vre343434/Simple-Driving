6474f6509b761cdbf3e10d3499ffc3130216fc83
nanaco541 <vre343434@gmail.com>
2021-09-21 20:10:29 +0800

    add EnergySystem

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,16 +9,58 @@ public class MainMenu : MonoBehaviour
 {
 
     [SerializeField] private TMP_Text highScoreText;
+    [SerializeField] private TMP_Text energyText;
+    [SerializeField] private TMP_Text nextTimePlay;
+    [SerializeField] private int maxEnergy;
+    [SerializeField] private int energyRechargeDuration;
+
+    private int energy;
+
+    private const string EnergyKey = "Energy";
+    private const string EnergyReadyKey = "EnergyReady";
 
     private void Start()
     {
         int highScore = PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0);
 
         highScoreText.text = $"High Score {highScore}";
+
+        energy = PlayerPrefs.GetInt(EnergyKey, maxEnergy);
+
+        if (energy == 0)
+        {
+            string energyReadyString = PlayerPrefs.GetString(EnergyReadyKey, string.Empty);
+
+            if (energyReadyString == string.Empty) { return; }
+
+            DateTime energyReady = DateTime.Parse(energyReadyString);
+
+            if (DateTime.Now > energyReady)
+            {
+                energy = maxEnergy;
+                PlayerPrefs.SetInt(EnergyKey, energy);
+            }
+
+        }
+
+        energyText.text = $"Play ({energy})";
+        nextTimePlay.text = $"NextTimePlay: {PlayerPrefs.GetString(EnergyReadyKey, string.Empty)}";
     }
 
     public void Play()
     {
+        if (energy < 1) { return; }
+
+        energy--;
+
+        PlayerPrefs.SetInt(EnergyKey, energy);
+
+        if (energy == 0)
+        {
+            DateTime energyReady = DateTime.Now.AddMinutes(energyRechargeDuration);
+            PlayerPrefs.SetString(EnergyReadyKey, energyReady.ToString());
+        }
+
         SceneManager.LoadScene(1);
     }
 
