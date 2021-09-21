8390f2c7cc919cefea55cc58d31c9f2e6629bd76
nanaco541 <vre343434@gmail.com>
2021-09-21 17:04:01 +0800

    test PlayerPrefs

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -8,6 +8,8 @@ public class ScoreSystem : MonoBehaviour
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private float scoreMultiplier;
 
+    public const string HighScoreKey = "HighScore";
+
     private float score;
 
     // Update is called once per frame
@@ -16,5 +18,15 @@ public class ScoreSystem : MonoBehaviour
         score += Time.deltaTime * scoreMultiplier;
 
         scoreText.text = Mathf.FloorToInt(score).ToString();
+    }
+
+    private void OnDestroy()
+    {
+        int currentHighScore = PlayerPrefs.GetInt(HighScoreKey, 0);
+
+        if (score > currentHighScore)
+        {
+            PlayerPrefs.SetInt(HighScoreKey, Mathf.FloorToInt(score));
+        }
     }
 }
